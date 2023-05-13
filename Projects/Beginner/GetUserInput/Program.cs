a8c69b1318ac1034b99244b03ea2ae3bfb0ae567
CosmicPhoenix2029 <haydnatkinson6@gmail.com>
2023-05-13 17:42:26 +0100

    Created the GetUserInput program.

---
+++ Projects/Beginner/GetUserInput/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("How old are you?");
+string userInput = Console.ReadLine();
+Console.WriteLine("You are " + userInput + " years old!");
+
+//Alternate method of concatenating the string
+//Console.Writeline($"You are {userInput} years old!")
