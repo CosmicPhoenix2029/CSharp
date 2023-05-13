2e5b59616ead7d06bb473faa76dd3af439f577e6
CosmicPhoenix2029 <haydnatkinson6@gmail.com>
2023-05-13 17:57:25 +0100

    Created a program to display the basic datatypes.

---
+++ Projects/Beginner/DataTypes/Program.cs
@@ -0,0 +1,11 @@
+﻿//variable declaration
+string text = "here is some text.";
+int wholeNumber = 5;
+double decimalNumber = 65.22;
+bool trueOrFalse = true;
+
+//output the variables to the console
+Console.WriteLine($"This is an example of a String: {text}");
+Console.WriteLine($"This is an example of an Integer: {wholeNumber}");
+Console.WriteLine($"This is an example of a Decimal: {decimalNumber}");
+Console.WriteLine($"This is an example of a boolean: {trueOrFalse}");
