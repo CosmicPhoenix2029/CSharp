bdaa7342611dc1daba8ac3a94b68dc47924261b4
CosmicPhoenix2029 <haydnatkinson6@gmail.com>
2023-05-15 10:52:13 +0100

    Created a program demonstrating a usecase of the Modulo Operator.

---
+++ Projects/Beginner/moduloOperator/Program.cs
@@ -0,0 +1,9 @@
+﻿//modulo is used to calculate remainders
+
+//this is useful when testing if a number fits into a larger number, or if there is a remainder
+
+int numberOfStudents = 18;
+int groupSize = 3;
+
+//using Modulo to test that there will be an even number of students in each group
+Console.WriteLine(numberOfStudents % groupSize);
