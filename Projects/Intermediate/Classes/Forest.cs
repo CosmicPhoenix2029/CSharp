d0b4f3573632ecc8a4a68e6e1d9831e3261eb3fd
CosmicPhoenix2029 <haydnatkinson6@gmail.com>
2023-06-03 16:36:44 +0100

    created a program demonstrating the key basic classes concepts

---
+++ Projects/Intermediate/Classes/Forest.cs
@@ -0,0 +1,70 @@
+﻿namespace Classes 
+{
+    public class Forest
+    {
+        //declaring some fields --best practice to use lowercase 
+        private string name;
+        private string biome;
+        private int age;
+
+        //here is an example of a constructor
+        public Forest(string name, string biome)
+        { 
+            //using the "this" keyword to show that the properties are set on the specific instance of the Forest class
+            this.Name = name;
+            this.Biome = biome;
+            Age = 0;
+        }
+
+        /*overloading the constructor:
+         - this constructor is used when only the first parameter was passed in when creating a new Forest object
+         - Using an optional parameter (string name = "unknown") in this case is not possible as there is additional behavior in the overloaded constructor
+         - Note here we are using the ": this" syntax to reference the original constructor
+         - This will call the original constructor, passing in the provided name parameter, and a default value of unknown as the "biome" parameter*/
+        public Forest(string name) : this(name, "unknown")
+        {
+            Console.WriteLine("WARNING! name not provided. Defaulting the value to unknown.");               
+        }
+
+        //basic property with no input validation --best practice is uppercase
+        public string Name 
+        {
+            get { return name; }
+            //this uses the value keyword to set the value of name to the provided value
+            set { name = value; }
+        }
+
+        //property with input validation:
+        public string Biome 
+        {
+            get { return biome; }
+            //using the ternary operator to make it cleaner than an if or switch case
+            set { biome = (value == "Tropical" || value == "Temperate" || value == "Boreal") ? value : "Unknown"; }
+        }
+        public int Trees
+        {
+            //shorthand syntax (automatic property)
+            get; set;
+        }
+        //read only method (when accessing outside of this class)
+        public int Age
+        {
+            get { return age; }
+            private set { age = value; }
+        }
+
+        public int Grow()
+        {
+            Trees = Trees + 30;
+            Age++;
+            return Trees;
+        }
+
+        public int Burn()
+        {
+            Trees = Trees - 20;
+            Age++;
+            return Trees;
+        }
+    }
+}
---
+++ Projects/Intermediate/Classes/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using another class (creating an instance of a class)
+            Forest forest = new Forest();
+            //setting the instances fields
+            forest.Name = "Amazon";
+            forest.Biome = "tropical rainforest";
+            forest.Trees = 10400653;
+            forest.Age = 4400;
+        }
+    } 
+}
