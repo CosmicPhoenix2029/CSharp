9954bcda9de365a370ec2b2b2f90212db4b6d0ec
CosmicPhoenix2029 <haydnatkinson6@gmail.com>
2023-06-04 10:40:00 +0100

    basic program to create user profiles (reinforcing classes knwoledge)

---
+++ Projects/Intermediate/UserProfiles/Profile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Profile
+{
+    //fields
+    private string name;
+    private int age;
+    private string city;
+    private string country;
+    private string pronouns;
+    private string[] hobbies;
+
+    //constructor
+    public Profile(string name, int age, string city, string country, string pronouns = "they/them")
+	{
+        this.name = name;
+        this.age = age;
+        this.city = city;
+        this.country = country;
+        this.pronouns = pronouns;
+        this.hobbies = new string[] { };
+	}
+
+    //methods
+    public string ViewProfile() 
+    {
+        return $"Name: {this.name}\nAge: {this.age}\nCity: {this.city}\nCountry: {this.country}\nPronouns: {this.pronouns}\nHobbies: \n - {String.Join("\n - ", this.hobbies)}";   
+    }
+
+    public void SetHobbies(string[] hobbies)
+    { 
+        this.hobbies = hobbies;
+    }
+}
---
+++ Projects/Intermediate/UserProfiles/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ !CODEACADEMY PROJECT!
+ This program will allow users to create there own profiles with the following information:
+ - name
+ - age
+ - city
+ - country
+ - pronouns
+ - hobbies
+
+This will allow the user perform the follwing actions:
+ - create a new profile
+ - add hobbies
+ - view profile
+ */
+
+namespace UserProfiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile sam = new Profile("Sam Drakkila", 30, "New York", "USA", "he/him");
+            //here using the new string[] we can build an array inside a method call
+            sam.SetHobbies(new string[] {"listening to audiobooks and podcasts", "playing rec sports like bowling and kickball", "writing a speculative fiction novel", "reading advice columns"});
+            Console.WriteLine(sam.ViewProfile());
+        }
+    }
+}
