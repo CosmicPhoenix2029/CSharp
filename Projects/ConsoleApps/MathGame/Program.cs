c4824b000042721e421730f6356450b2531cbcf6
CosmicPhoenix2029 <haydnatkinson6@gmail.com>
2023-06-11 10:49:53 +0100

    created a maths game project

---
+++ Projects/ConsoleApps/MathGame/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program 
+{
+    static void main()
+    {
+        Console.WriteLine(GameModeSelector);    
+    }
+    public static string GameModeSelector()
+    {
+        //the @ symbol makes it so the string is treated as "verbatim" (as displayed in the IDE)
+        Console.WriteLine(@$"Welcome to my maths game! Please enter an option from the list below:
+        A - Addition    
+        S - Subtraction
+        M - Multiplication
+        D - Division
+        R - Random");
+        return Console.ReadLine();
+    }
+}
