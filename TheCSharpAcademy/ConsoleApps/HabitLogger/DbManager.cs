6b2541b0fbab125197437e873a04c5bebf05d36f
CosmicPhoenix2029 <haydnatkinson6@gmail.com>
2023-06-22 17:57:20 +0100

    started creating the CRUD operations

--- TheCSharpAcademy/ConsoleApps/HabitLogger/DbManager.cs
+++ TheCSharpAcademy/ConsoleApps/HabitLogger/DbManager.cs
@@ -29,16 +29,90 @@ public class DbManager
         connection.Open();
 
         SQLiteCommand command = connection.CreateCommand();
-        command.CommandText = @"CREATE TABLE IF NOT EXISTS runs (
-                                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                                    distance VARCHAR NOT NULL,
-                                    timeTaken VARCHAR,
-                                    Date DATETIME
-                                );";
+        command.CommandText = 
+            @"
+                CREATE TABLE IF NOT EXISTS runs (
+                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    distance VARCHAR NOT NULL,
+                    timeTaken VARCHAR NOT NULL,
+                    Date DATETIME
+                )
+            ;";
 
         try { command.ExecuteNonQuery(); }
         catch (SQLiteException error){ Console.WriteLine(error.Message); }
+        finally { connection.Close(); }  
+    }
+
+    public void CreateEntry(string distance, string timeTaken, DateTime date) 
+    {
+        using var connection = new SQLiteConnection($"Data Source={_DbName}");
+        //create the table within the db if it doesn't exist:
+        connection.Open();
+
+        SQLiteCommand command = connection.CreateCommand();
+        command.CommandText = 
+            @$"
+                INSERT INTO runs
+                VALUES ({distance}, {timeTaken}, {date})
+            ";
+        //need to add a bit here to check an existing entry with the same parameters doesn't already exist.
+
+        try { command.ExecuteNonQuery(); }
+        catch (SQLiteException error) { Console.WriteLine(error.Message); }
         finally { connection.Close(); }
-        
+    }
+
+    public void ReadEntry()
+    {
+        using var connection = new SQLiteConnection($"Data Source={_DbName}");
+        //create the table within the db if it doesn't exist:
+        connection.Open();
+
+        SQLiteCommand command = connection.CreateCommand();
+        command.CommandText = 
+            @"
+                SELECT * FROM runs
+                WHERE 
+            ";
+
+        try { command.ExecuteNonQuery(); }
+        catch (SQLiteException error) { Console.WriteLine(error.Message); }
+        finally { connection.Close(); }
+    }
+
+    public void UpdateEntry(string distance, string time)
+    {
+        using var connection = new SQLiteConnection($"Data Source={_DbName}");
+        //create the table within the db if it doesn't exist:
+        connection.Open();
+
+        SQLiteCommand command = connection.CreateCommand();
+        command.CommandText = 
+            @$"
+                UPDATE runs
+                SET distance = {distance}, time = {time}
+            ";
+
+        try { command.ExecuteNonQuery(); }
+        catch (SQLiteException error) { Console.WriteLine(error.Message); }
+        finally { connection.Close(); }
+    }
+
+    public void DeleteEntry(string distance, string time) 
+    {
+        using var connection = new SQLiteConnection($"Data Source={_DbName}");
+        //create the table within the db if it doesn't exist:
+        connection.Open();
+
+        SQLiteCommand command = connection.CreateCommand();
+        command.CommandText = 
+            @$"
+                DELETE FROM runs WHERE distance = {distance} AND time = {time}
+            ";
+
+        try { command.ExecuteNonQuery(); }
+        catch (SQLiteException error) { Console.WriteLine(error.Message); }
+        finally { connection.Close(); }
     }
 }
